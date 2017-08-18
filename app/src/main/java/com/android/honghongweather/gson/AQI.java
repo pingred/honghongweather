ba1f5c4cede0dab3a9a4a72a225d6596dbe4aba3
Pingred <pingred@gmail.com>
2017-08-18 14:50:30 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/android/honghongweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.android.honghongweather.gson;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
